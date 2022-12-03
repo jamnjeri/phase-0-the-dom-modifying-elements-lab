8bc841b481a2a99a10d4cb1e71c7feedb67272a9
Jamila <jamnjeri@gmail.com>
2022-12-03 11:45:44 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+//access the element
+const element = document.getElementById("main")
+
+//Delete the element
+element.remove()
+
+// //Add a newHeader variable that points to a h1 node
+const newHeader = document.createElement("h1")
+newHeader.id = 'victory'
+newHeader.textContent = 'Jamila is the champion';
+document.body.append(newHeader)
